7ad68f98eda9ec52c2ff705ca598e81053cc52c9
Gulshan Kumar <gulshankumar@Gulshans-MacBook-Air.local>
2024-11-01 01:24:29 +0530

    Navbar completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,40 +3,38 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
+import musicData from "../data/music_courses.json"
 
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}> 
       <Menu setActive={setActive}>
         <Link href="/">
-          <MenuItem setActive={setActive} item="Home">
+          <MenuItem setActive={setActive} active={active} item="Home">
           </MenuItem>
         
         </Link>
+     
 
         <MenuItem setActive={setActive} active={active} item="Courses">
           <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="/courses">
-                Guitar Fundamentals
+            {
+              musicData.map((course) => (
+                <HoveredLink key={course.id} href={`/courses`}>
+                  {course.title}
+                </HoveredLink>
+              ))
+            }
               
-              </HoveredLink>
-              <HoveredLink href="/courses">Piano Fundamentals</HoveredLink>
-              <HoveredLink href="/courses">Vocal Training</HoveredLink>
-              <HoveredLink href="/courses">Music Production Basics</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
+        <Link href="/contact">
+          <MenuItem setActive={setActive} active={active} item="Contact Us">
+          </MenuItem>
+        
+        </Link>
       </Menu>
     </div>
   );
